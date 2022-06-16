59e5373eb5338c325e37fff9f749e2f37589d9c6
kjh9571 <jhkim9571@gmail.com>
2022-06-16 16:53:37 +0900

    Project folder for collaboration

--- project/src/model.py
+++ project/src/model.py
@@ -1,8 +1,9 @@
 from sklearn.metrics import mean_absolute_error
 
 def training(model, X, y):
-    model = model
-    model.fit(X, y)
-    pred_train = model.predict(X)
+    reg = model
+    reg.fit(X, y)
+    pred_train = reg.predict(X)
     mae_train = mean_absolute_error(y, pred_train)
-    return mae_train
+    return [mae_train, reg]
+    
